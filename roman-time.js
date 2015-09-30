d9d41ac4c4596cad4716ba84e93ea55e11129b7a
Kirill <krisstal.mail@gmail.com>
2015-09-30 23:52:42 +0500

    init

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,20 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-// Немного замечательного кода и магии
+var figlet = require('figlet');
+var toRoman = require('roman-numerals').toRoman;
+
+function isTime(n) {
+  return n > 0 && n <= 60; // нету символа для 0
+}
+
+if (isTime(hours) && isTime(minutes))
+  figlet(
+    [toRoman(hours), ':', toRoman(minutes)].join(''),
+    'Dot Matrix',
+    function (err, data) {
+      console.log(err || data);
+    }
+  );
+else
+  console.log('Время указано не верно');
